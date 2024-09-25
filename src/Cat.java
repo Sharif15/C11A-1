7dd16fcd00994c8fe25de811269d3f5487a6da94
Sharif15 <Sharifpial225@gmail.com>
2024-09-25 12:39:26 -0600

    changed the eye dimension and message position

--- src/Cat.java
+++ src/Cat.java
@@ -11,8 +11,8 @@ public class Cat {
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 20;
-	private static final int EYE_WIDTH = 5;
+	private static final int EYE_HEIGHT = 18;
+	private static final int EYE_WIDTH = 8;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
@@ -42,8 +42,8 @@ public class Cat {
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
-		// Meow text appears below cat head, +10 places below 
+		// Meow text appears above cat head, +10 places above 
 		// so it doesn't overlap the drawing
-		g2.drawString(">.<", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString(">.<", catX, catY-10);	
 	}
 }
